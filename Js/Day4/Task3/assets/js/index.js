9379407e764d5c5524f072b58da0c535b70f7fd7
Riswana Barvin B <riswanabarvin643@gmail.com>
2026-09-24 18:01:23 +0530

    updated

---
+++ Js/Day4/Task3/assets/js/index.js
@@ -0,0 +1,23 @@
+
+// task1
+
+let number=10;
+
+if (number > 0) {
+    console.log("Positive Number");
+}
+
+// task2
+
+let age = 20;
+
+if (age >= 18) {
+    console.log("Eligible to Vote");
+} else {
+    console.log("Not Eligible to Vote");
+}
+
+// task3
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}   
